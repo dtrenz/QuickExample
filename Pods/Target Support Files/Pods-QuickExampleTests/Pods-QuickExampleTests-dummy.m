63a969f73dcada99afa574aae5a646b2c07518e3
Dan Trenz <dtrenz@gmail.com>
2016-05-26 16:10:46 -0400

    First commit

---
+++ Pods/Target Support Files/Pods-QuickExampleTests/Pods-QuickExampleTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_QuickExampleTests : NSObject
+@end
+@implementation PodsDummy_Pods_QuickExampleTests
+@end
---
+++ QuickExampleTests/ExampleSpec.swift
@@ -0,0 +1,12 @@
+import Quick
+import Nimble
+
+class ExampleSpec: QuickSpec {
+    override func spec() {
+        describe("Test Spec") {
+            it("should execute and fail this test") {
+                expect(true) == false
+            }
+        }
+    }
+}
